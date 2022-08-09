2b5ae104b286ec2cf37ba752a8bd257135f9dcd6
Evgeniy Husainov <evares92@gmail.com>
2022-08-09 08:47:52 +0300

    Работа со статикой 1.0

--- README.md
+++ README.md
@@ -103,6 +103,6 @@ Python3, Django, HTTP, Django Rest Framework, PostgreSQL, Docker, YandexCloud
 # Авторы
 ​
 - Хусаинов Евгений Маратович (Exusainov@yandex.com)
-​- IP Адрес 84.252.142.211
+​- IP Адрес 84.201.136.214
 - login evares92@gmail.com  
 - Pass admin
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = "p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['84.252.142.211', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['84.201.136.214', '127.0.0.1', 'localhost']
 
 
 # Application definition
